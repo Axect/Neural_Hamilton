7b31729adb864331a3094d2dd299de39355990d5
Axect <axect.tg@proton.me>
2024-11-16 16:28:13 +0900

    Remove unnecessary script

--- scripts/util.py
+++
@@ -1,8 +0,0 @@
-import polars as pl
-import numpy as np
-
-
-def load_data(file_path: str):
-    df = pl.read_parquet(file_path)
-    tensors = [df[col].to_numpy().reshape(-1, 100) for col in df.columns]
-    return np.array(tensors)
