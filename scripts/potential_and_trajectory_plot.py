11f7af1a4420f1f365fa39cdcc65f67c03902cd4
axect <axect.tg@proton.me>
2025-01-05 19:59:27 +0900

    Potential scale fix

--- scripts/potential_and_trajectory_plot.py
+++ scripts/potential_and_trajectory_plot.py
@@ -1,31 +1,30 @@
 import matplotlib.pyplot as plt
 import scienceplots
 import numpy as np
+import sys
+
+sys.path.append("../")
 
 from util import load_data
 
 # Load data
-data = load_data("../data_normal/val.parquet")
-print(data.shape) # pyright:ignore
-V_total = data[0]
-t_total = data[1]
-x_total = data[2]
-p_total = data[3]
-
-x_domain = np.linspace(0, 1, V_total.shape[1])
+data = load_data("../data_test/test.parquet")
+q_domain = np.linspace(0, 1, 100)
 
 for i in range(10):
+    V, t, q, p = data[i]
     with plt.style.context(["science", "nature"]):
         fig, ax = plt.subplots()
-        ax.plot(x_domain, V_total[i])
+        ax.plot(q_domain, V)
         ax.set_xlabel(r"$q$", fontsize=8)
         ax.set_ylabel(r"$V(q)$", fontsize=8)
         ax.autoscale(tight=True)
+        ax.set_ylim((-1, 2))
         fig.savefig(f"../figs/00_V_{i}.png", dpi=600, bbox_inches="tight")
         plt.close(fig)
 
         fig, ax = plt.subplots()
-        ax.plot(t_total[i], x_total[i])
+        ax.plot(t, q)
         ax.set_xlabel(r"$t$", fontsize=8)
         ax.set_ylabel(r"$q(t)$", fontsize=8)
         ax.autoscale(tight=True)
@@ -33,7 +32,7 @@ for i in range(10):
         plt.close(fig)
 
         fig, ax = plt.subplots()
-        ax.plot(t_total[i], p_total[i])
+        ax.plot(t, p)
         ax.set_xlabel(r"$t$", fontsize=8)
         ax.set_ylabel(r"$p(t)$", fontsize=8)
         ax.autoscale(tight=True)
