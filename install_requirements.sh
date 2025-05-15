4282e951f0c9e11073254023bb715958337ac593
axect <axect.tg@proton.me>
2025-05-15 17:31:30 +0900

    Update install script

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,1 +1,1 @@
-uv pip install -U torch wandb polars numpy optuna matplotlib scienceplots beaupy rich mambapy scipy
+uv pip install -U torch wandb polars numpy optuna matplotlib scienceplots beaupy rich mambapy scipy "umap-learn[plot]" scikit-learn
